7e3538b0c961f83d5dbdd5e183542f9cd11e0774
MeghlaB <meghlab964@gmail.com>
2024-12-05 21:28:40 +0600

    myCampaign added

--- src/Components/MyCompaign.jsx
+++ src/Components/MyCompaign.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { Link, useLoaderData } from 'react-router-dom'
 import { AuthContext } from '../AddProvider/AuthProvider';
 import Swal from 'sweetalert2';
@@ -7,28 +7,74 @@ import CompaignCard from './CompaignCard';
 export default function MyCampaign() {
   const{user}= useContext(AuthContext)
   const campaignData = useLoaderData();
-  // console.log(campaignData)
-  const [userCampaign , setUserCampaign] = useState([])
-  useEffect(()=>{
-    setUserCampaign(campaignData?.filter((data) => data.email === user?.email))
- },[campaignData,setUserCampaign])
-
-const handleDelete = (_id)=>{
-    setUserCampaign((data)=>{userCampaign.filter ((data)=> data._id !== _id)
-
-})
+const userCampaigns = campaignData.filter((data) => data.email === user?.email)
+const [comapign , setComapaign ] = useState(userCampaigns)
+// console.log(userCampaigns)
+const handleDelete = _id =>{
+  console.log(_id)
+  Swal.fire({
+    title: "Are you sure?",
+    text: "Campaign card is Deleted!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Yes, delete it!"
+  }).then((result) => {
+    if (result.isConfirmed) {
+      fetch(`http://localhost:5000/addCompaign/${_id}`,{
+        method:"DELETE"
+      })
+      .then((res)=>res.json())
+      .then(data=>{
+        if(data.deletedCount >0){
+          Swal.fire({
+            title: "Deleted!",
+            text: "Your file has been deleted.",
+            icon: "success"
+          });
+          const remaining = comapign.filter ((data)=> data._id !== _id)
+          setComapaign(remaining)
+        }
+      })
+    }
+  });
 }
   return (
-    
-    <div className='conatiner mx-auto my-5'>
-      <div className='grid grid-cols-1 lg:grid-cols-3  gap-5 '>
-      {
-        userCampaign?.map((campaign)=>{
-         return <CompaignCard key={campaign._id} campaign={campaign} onClick={handleDelete}></CompaignCard>
-         })
-      }
-    </div>
- 
-    </div>
+    <div className="container mx-auto my-5">
+<div className="overflow-x-auto">
+  <table className="table ">
+    <thead>
+      <tr>
+        <th></th>
+        <th>Title</th>
+        <th>Date</th>
+        <th>MinDonation</th>
+        <th>Update</th>
+        <th>Delete</th>
+      </tr>
+    </thead>
+    <tbody>
+      {comapign.map((data, index) => (
+        <tr key={data._id}> 
+          <th>{index + 1}</th>
+          <td>{data.title}</td>
+          <td>{data.deadline}</td>
+          <td>{data.minDonation}</td>
+          <td>
+            <Link to={`/update/${data._id}`} className="btn btn-primary">Update</Link>
+          </td>
+          <td>
+            <Link 
+            onClick={()=> handleDelete(data._id)}
+             className="btn btn-error">Delete</Link>
+          </td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+</div>
+</div> 
   );
 }
+{/* */}
